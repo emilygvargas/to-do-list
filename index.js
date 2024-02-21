2e031507ea930e9ca22c51c2dc8303755f12350d
Avery <evans.c.avery@gmail.com>
2024-02-21 14:23:06 -0700

    finished stretch goals

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 console.log('my code is running')
+retrieveList();
 
+function retrieveList(){
+  if(localStorage.length < 1){
+    return null;
+  }
+  else{
+    for(let i=0; i<localStorage.length; i++){
+      var retrievedItem = localStorage.getItem(localStorage.key(i));
+
+      const taskName = document.createElement("li");
+      taskName.innerHTML = retrievedItem;
+
+      const checkmarkButton = document.createElement("button");
+      checkmarkButton.className="Checkmark";
+      checkmarkButton.setAttribute("onclick", "completed(this)");
+
+      const removeButton = document.createElement("button");
+      removeButton.className="Remove-Task";
+      removeButton.setAttribute("onclick", "remove(this)");
+      removeButton.innerHTML = "Remove";
+
+
+      let newItem = document.createElement("div");
+      newItem.className="To-Do-Item";
+      newItem.appendChild(checkmarkButton);
+      newItem.appendChild(taskName);
+      newItem.appendChild(removeButton);
+
+     document.getElementById("To-Do-List").appendChild(newItem);
+
+    }
+  }
+}
 
 let toDoList;
 
@@ -9,6 +42,11 @@ function completed(event){
   let itemDiv = event.parentNode;
   let listItem = itemDiv.querySelector("li");
   listItem.innerHTML = '<del>'+ listItem.innerHTML + '</del>';
+
+  let archiveParent = document.getElementById("Archive");
+  archiveParent.appendChild(listItem);
+
+  remove(event);
 }
 function remove(event){
   let itemDiv = event.parentNode;
@@ -36,12 +74,14 @@ function newElement(){
   removeButton.setAttribute("onclick", "remove(this)");
   removeButton.innerHTML = "Remove";
 
-  let div = document.createElement("div");
-  div.className="To-Do-Item";
-  div.appendChild(checkmarkButton);
-  div.appendChild(taskName);
-  div.appendChild(removeButton);
+  let newItem = document.createElement("div");
+  newItem.className="To-Do-Item";
+  newItem.appendChild(checkmarkButton);
+  newItem.appendChild(taskName);
+  newItem.appendChild(removeButton);
 
-  document.getElementById("To-Do-List").appendChild(div);
+  localStorage.setItem(inputValue, inputValue);
+  console.log(localStorage);
 
+  document.getElementById("To-Do-List").appendChild(newItem);
 }
